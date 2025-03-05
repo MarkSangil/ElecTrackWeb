f53f841123ab3920f991911522920ac61c92c93e
MarkSangil <marksangil.bsit2019@gmail.com>
2025-03-05 22:32:09 +0800

    Deploy Flutter web build

--- lib/color_extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-extension WithValues on Color {
-  /// Returns a new Color with the same red, green, and blue values,
-  /// but with the provided alpha value (0.0 to 1.0).
-  Color withValues({required double alpha}) {
-    return Color.fromRGBO(red, green, blue, alpha);
-  }
-}
